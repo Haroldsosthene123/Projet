34df477d9be635dc5161b0912905128ddf5b12fd
Haroldsosthene123 <you@example.com>
2023-08-17 00:01:37 -0800

    first commit

---
+++ Routes/allRoutes.js
@@ -0,0 +1,349 @@
+//Dashboard
+/* import DashboardAnalytics from "../pages/DashboardAnalytics";
+import DashboardCrm from "../pages/DashboardCrm";
+import DashboardEcommerce from "../pages/DashboardEcommerce";
+
+import DashboardCrypto from "../pages/DashboardCrypto";
+// import DashboardProject from "../pages/DashboardProject";
+import DashboardNFT from "../pages/DashboardNFT";
+
+//Crm Pages
+import CrmCompanies from "../pages/Crm/CrmCompanies";
+// import CrmContacts from "../pages/Crm/CrmContacts";
+import CrmParticuliers from "../pages/Crm/CrmParticuliers";
+import TypeAbonnementList from "../pages/Abonnement/TypeAbonnementList";
+import SuccessMsgAbo from "../pages/Abonnement/SuccessMsgAbo";
+import AbonnementInfo from "../pages/Abonnement/AbonnementInfo";
+import CrmEntreprises from "../pages/Crm/Entreprises/CrmEntreprises";
+import CrmDeals from "../pages/Crm/CrmDeals/index";
+import CrmLeads from "../pages/Crm/CrmLeads/index";
+
+//Ventes Pages
+import Ventes from "../pages/Ventes/index";
+import VenteAddNewVente from "../pages/Ventes/VenteAddNewVente";
+import VenteUpdateVente from "../pages/Ventes/VenteUpdateVente";
+import VenteDeleteVente from "../pages/Ventes/VenteDeleteVente";
+
+//Depenses Pages
+import Depenses from "../pages/Depenses/index";
+import DepenseAddNew from"../pages/Depenses/DepenseAddNew";
+import DepenseDetails from "../pages/Depenses/DepenseDetails";
+import DepenseUpdate from"../pages/Depenses/DepenseUpdate";
+
+
+//Achat Pages
+import Achats from "../pages/Achats/index";
+import AchatsAddNew from "../pages/Achats/AchatsAddNew";
+import AchatsUpdate from "../pages/Achats/AchatsUpdate";
+import AchatsDetails from "../pages/Achats/AchatsDetails";
+
+//Avoirs Pages
+import Avoirs from "../pages/Documents/Avoirs/index";
+import AvoirsAdd from "../pages/Documents/Avoirs/AvoirsAdd";
+import AvoirsView from "../pages/Documents/Avoirs/AvoirsView";
+
+//Devis Pages
+import Devis from "../pages/Documents/Devis/index"; 
+import DevisAdd from "../pages/Documents/Devis/DevisAdd"; 
+import DevisView from "../pages/Documents/Devis/DevisView";
+
+//Factures Pages
+import Factures from "../pages/Documents/Factures";
+import FactureAdd from "../pages/Documents/Factures/FactureAdd";
+import FactureView from "../pages/Documents/Factures/FactureView"
+
+//Factures Proformas Pages 
+import FactureProformas from "../pages/Documents/FactureProforma";
+import FactureProformaAdd from "../pages/Documents/FactureProforma/FactureProformaAdd";
+import FactureProformaView from "../pages/Documents/FactureProforma/FactureProformaView";
+
+//Bon de Commande Pages
+import BonCommande from "../pages/Documents/BonCommande";
+import BonCommandeAdd from "../pages/Documents/BonCommande/BonCommandeAdd";
+import BonCommandeView from "../pages/Documents/BonCommande/BonCommandeView";
+
+//Bon de Livraison Pages
+import BonLivraison from "../pages/Documents/BonLivraison";
+import BonLivraisonAdd from "../pages/Documents/BonLivraison/BonLivraisonAdd";
+import BonLivraisonView from "../pages/Documents/BonLivraison/BonLivraisonView";
+
+//Abonnement Pages
+import Pricing from "../pages/Abonnement/Pricing";
+//Team Pages
+import Team from "../pages/Pages/Team/Team";
+import Calendar from "../pages/Calendar"; */
+
+//AuthenticationInner pages
+import BasicSignIn from '../pages/AuthenticationInner/Login/BasicSignIn';
+/* import CoverSignIn from '../pages/AuthenticationInner/Login/CoverSignIn';
+import BasicSignUp from '../pages/AuthenticationInner/Register/BasicSignUp';
+import CoverSignUp from "../pages/AuthenticationInner/Register/CoverSignUp";
+import CoverSignUpEn from "../pages/AuthenticationInner/Register/CoverSignUpEn";
+import BasicPasswReset from '../pages/AuthenticationInner/PasswordReset/BasicPasswReset';
+import AffiliateSignUp from "../pages/AuthenticationInner/Affiliate/AffiliateSignUp";
+import AffiliateSignUpEn from "../pages/AuthenticationInner/Affiliate/AffiliateSignUpEn";
+import AffiliateSignIn from "../pages/AuthenticationInner/Affiliate/AffiliateSignIn";
+import AffiliateSignInEn from "../pages/AuthenticationInner/Affiliate/AffiliateSignInEn";
+import AffiliateDashboard from "../pages/AuthenticationInner/Affiliate/AffiliateDashboard"; */
+//Ecommerce Pages
+/* import EcommerceProducts from "../pages/Ecommerce/EcommerceProducts/index";
+import EcommerceServices from "../pages/Ecommerce/EcommerceServices/index";
+import EcommerceCategories from "../pages/Ecommerce/EcommerceCategories/index";
+import EcommerceTypes from "../pages/Ecommerce/EcommerceTypes/index";
+import EcommerceProductDetail from "../pages/Ecommerce/EcommerceProducts/EcommerceProductDetail";
+import EcommerceServiceDetail from "../pages/Ecommerce/EcommerceServices/EcommerceServiceDetail";
+import EcommerceAddProduct from "../pages/Ecommerce/EcommerceProducts/EcommerceAddProduct";
+import EcommerceAddService from "../pages/Ecommerce/EcommerceServices/EcommerceAddService";
+import EcommerceUpdateProduct from "../pages/Ecommerce/EcommerceProducts/EcommerceUpdateProduct";
+import EcommerceUpdateService from "../pages/Ecommerce/EcommerceServices/EcommerceUpdateService";
+import EcommerceAddCategories from "../pages/Ecommerce/EcommerceCategories/EcommerceAddCategories";
+import EcommerceAddTypes from "../pages/Ecommerce/EcommerceTypes/EcommerceAddTypes";
+import EcommerceUpdateTypes from "../pages/Ecommerce/EcommerceTypes/EcommerceUpdateTypes";
+import EcommerceUpdateCategories from "../pages/Ecommerce/EcommerceCategories/EcommerceUpdateCategories";
+import EcommerceOrders from "../pages/Ecommerce/EcommerceOrders/index";
+import EcommerceOrderDetail from "../pages/Ecommerce/EcommerceOrders/EcommerceOrderDetail";
+import EcommerceCustomers from "../pages/Ecommerce/EcommerceCustomers/index";
+import EcommerceCart from "../pages/Ecommerce/EcommerceCart";
+import EcommerceCheckout from "../pages/Ecommerce/EcommerceCheckout";
+import EcommerceSellers from "../pages/Ecommerce/EcommerceSellers/index";
+import EcommerceSellerDetail from "../pages/Ecommerce/EcommerceSellers/EcommerceSellerDetail";
+
+//Task
+import TaskDetails from "../pages/Tasks/TaskDetails";
+import TaskList from "../pages/Tasks/TaskList";
+import KanbanBoard from "../pages/Tasks/KanbanBoard/Index";
+import Calender from "../pages/Calendar/index";
+
+//pages
+import CoverPasswReset from '../pages/AuthenticationInner/PasswordReset/CoverPasswReset';
+import BasicLockScreen from '../pages/AuthenticationInner/LockScreen/BasicLockScr';
+import CoverLockScreen from '../pages/AuthenticationInner/LockScreen/CoverLockScr';
+import BasicLogout from '../pages/AuthenticationInner/Logout/BasicLogout';
+import CoverLogout from '../pages/AuthenticationInner/Logout/CoverLogout';
+import BasicSuccessMsg from '../pages/AuthenticationInner/SuccessMessage/BasicSuccessMsg';
+import SuccessMsg from '../pages/AuthenticationInner/SuccessMessage/SuccessMsg';
+import SuccessMsgEn from '../pages/AuthenticationInner/SuccessMessage/SuccessMsgEn';
+import CoverSuccessMsg from '../pages/AuthenticationInner/SuccessMessage/CoverSuccessMsg';
+import BasicTwosVerify from '../pages/AuthenticationInner/TwoStepVerification/BasicTwosVerify';
+import CoverTwosVerify from '../pages/AuthenticationInner/TwoStepVerification/CoverTwosVerify';
+import Basic404 from '../pages/AuthenticationInner/Errors/Basic404';
+import Cover404 from '../pages/AuthenticationInner/Errors/Cover404';
+import Alt404 from '../pages/AuthenticationInner/Errors/Alt404';
+import Error500 from '../pages/AuthenticationInner/Errors/Error500';
+import BasicPasswCreate from "../pages/AuthenticationInner/PasswordCreate/BasicPasswCreate";
+import CoverPasswCreate from "../pages/AuthenticationInner/PasswordCreate/CoverPasswCreate";
+import Offlinepage from "../pages/AuthenticationInner/Errors/Offlinepage";
+
+import SimplePage from "../pages/Pages/Profile/SimplePage/SimplePage";
+import Settings from "../pages/Pages/Profile/Settings/Settings";
+import CompanieSettings from "../pages/Pages/Profile/Settings/CompanieSettings";
+import ContactsDetail from "../pages/Crm/ContactsDetail";
+import FileManager from "../pages/FileManager";
+import Stock from "../pages/stock/Stock";
+
+//login
+import Login from "../pages/Authentication/Login";
+import LoginEn from "../pages/Authentication/LoginEn";
+import LoginAdmin from "../pages/Authentication/LoginAdmin";
+import ForgetPasswordPage from "../pages/Authentication/ForgetPassword";
+import Activate from "../pages/Authentication/Activate";
+import Logout from "../pages/Authentication/Logout";
+import Register from "../pages/Authentication/Register";
+
+//Administration 
+import Abonnes from "../pages/Administration/Abonnes";
+
+// User Profile
+import UserProfile from "../pages/Authentication/user-profile";
+import ContactsDetailEn from "../pages/Crm/Entreprises/ContactsDetailEn";
+import AbonnesFormule from "../pages/Administration/AbonnesFormule";
+import SimplePageAdmin from "../pages/Administration/SimplePageAdmin";
+import AdminsDemos from "../pages/Administration/AdminsDemos";
+import DemosAdmins from "../pages/Administration/DemosAdmins";
+ */
+// Définissez les noms uniques pour chaque écran que vous souhaitez naviguer.
+export const authProtectedRoutes = [
+  { name: "DashboardAnalytics", component: DashboardAnalytics }, // Utilisez des noms significatifs
+  { name: "DashboardCrm", component: DashboardCrm },
+  { name: "DashboardEcommerce", component: DashboardEcommerce },
+  { name: "DashboardCrypto", component: DashbordCryto},
+  { name:"DashboardNFT", component: DashboardNFT},
+
+  //Gestions des stocks
+
+  { name:"Stocks", component: Stocks},
+
+  //Depenses
+  { name:"Depenses", component: Depenses},
+  { name:"DepenseAddNew", component: DepenseAddNew},
+  { name:"DepenseUpdate", component: DepenseUpdate},
+  { name:"DepenseDetails", component: DepenseDetails},
+
+  //Achats
+  { name:"Achats", component: Achats },
+  { name:"AchatsAddNew", component: AchatsAddNew },
+  { name:"AchatsUpdate", component: AchatsUpdate},
+  { name:"AchatsDetails", component: AchatsDetails },
+
+  //Ventes
+  { name: "Ventes", component: Ventes },
+  { name: "VenteAddNewVente", component: VenteAddNewVente },
+  { name: "VenteUpdateVente", component: VenteUpdateVente },
+  { name: "VenteDeleteVente", component: VenteDeleteVente },
+
+   //Devis 
+   { name: "Devis", component: Devis },
+   { name: "DevisAdd", component: DevisAdd },
+   { name: "DevisView", component: DevisView },
+
+    //Factures
+  { name: "Factures", component: Factures },
+  { name: "FactureAdd", component: FactureAdd },
+  { name: "FactureView", component: FactureView },
+
+    //Factures Proformas
+ { name: "FactureProformas ", component: FactureProformas },
+ { name: "FactureProformaAdd", component: FactureProformaAdd },
+  { name: "FactureProformaView", component: FactureProformaView },
+
+    //Avoirs
+  { name: "Avoirs", component: Avoirs },
+  { name: "AvoirsAdd", component: AvoirsAdd },
+  { name: "AvoirsAdd", component: AvoirsAdd },
+  { name: "AvoirsView", component: AvoirsView },
+
+  { name: "BonCommande", component: BonCommande },
+  { name: "BonCommandeAdd", component: BonCommandeAdd },
+  { name: "BonCommandeView", component: BonCommandeView},
+
+  { name: "BonLivraison", component: BonLivraison },
+  { name: "BonLivraisonAdd", component: BonLivraisonAdd },
+  { name: "BonLivraisonAdd ", component: BonLivraisonAdd },
+  { name: "BonLivraisonView", component: BonLivraisonView },
+
+   //Ecommerce
+   { name: "EcommerceProducts", component: EcommerceProducts },
+   { name: "EcommerceServices", component: EcommerceServices },
+   { name: "EcommerceTypes", component: EcommerceTypes },
+   { name: "EcommerceCategories", component: EcommerceCategories },
+   { name: " EcommerceProductDetail", component: EcommerceProductDetail },
+   { name: "EcommerceAddTypes", component: EcommerceAddTypes },
+   { name: "EcommerceServiceDetail", component: EcommerceServiceDetail },
+   { name: "EcommerceAddProduct", component: EcommerceAddProduct },
+   { name: "EcommerceAddService", component: EcommerceAddService },
+   { name: "EcommerceUpdateProduct", component: EcommerceUpdateProduct },
+   { name: "EcommerceUpdateTypes", component: EcommerceUpdateTypes },
+   { name: "EcommerceUpdateService", component: EcommerceUpdateService },
+   { name: "EcommerceAddCategories", component: EcommerceAddCategories },
+   { name: "EcommerceUpdateCategories", component: EcommerceUpdateCategories },
+   { name: "/apps-ecommerce-orders", component: EcommerceOrders },
+   { name: "EcommerceOrderDetail", component: EcommerceOrderDetail },
+   { name: "EcommerceCustomers", component: EcommerceCustomers },
+   { name: "EcommerceCart", component: EcommerceCart },
+   { name: "/apps-calendar", component: Calender },
+   { name: "EcommerceCheckout", component: EcommerceCheckout },
+   { name: "EcommerceSellers", component: EcommerceSellers },
+   { name: "EcommerceSellerDetail", component: EcommerceSellerDetail },
+
+   //Task
+  { name: "TaskList", component: TaskList },
+  { name: "TaskDetails", component: TaskDetails },
+  { name: "KanbanBoard", component: KanbanBoard },
+
+  // Type Abonnement
+  { name: "TypeAbonnementList", component: TypeAbonnementList },
+  
+  // Type Abonnement
+  { name: "/SuccessMsgAbo", component: SuccessMsgAbo },
+  
+  // Info Abonnement
+  { name: "AbonnementInfo", component: AbonnementInfo },
+
+  //Crm
+  // { path: "/apps-crm-contacts", component: CrmContacts },
+  { name: "CrmParticuliers", component: CrmParticuliers },
+  { name: "ContactsDetail", component: ContactsDetail },
+  { name: "CrmCompanies", component: CrmCompanies },
+  { name: " CompanieSettings", component: CompanieSettings },
+  { name: "CrmEntreprises", component: CrmEntreprises },
+  { name: "ContactsDetailEn", component: ContactsDetailEn },
+  // { path: "/apps-crm-deals", component: CrmDeals },
+  // { path: "/apps-crm-leads", component: CrmLeads },
+  { name: " SimplePage", component: SimplePage },
+  { name: "Settings", component: Settings },
+
+  //Abonnement
+  { name: "Pricing", component: Pricing },
+
+  // //Gestionnaire de Fichier
+  // { path: "/apps-file-manager", component: FileManager },
+
+  //Team
+  { name: "Team", component: Team },
+  { name: "AdminsDemos", component: AdminsDemos },
+  { name: "DemosAdmins", component: DemosAdmins },
+
+   //User Profile
+   { name: "UserProfile", component: UserProfile },
+
+   //Administration Access
+   { name: "Abonnes", component: Abonnes },
+   { name: "AbonnesFormule", component: AbonnesFormule },
+   { name: "SimplePageAdmin", component: SimplePageAdmin },
+ 
+   // this route should be at the end of all other routes
+   // eslint-disable-next-line react/display-name
+//    {
+//      path: "/",
+//      exact: true,
+//      component: () => <Redirect to="/dashboard" />,
+//    },
+
+];
+
+export const affiliateProtectedRoutes = [
+  { name: "AffiliateDashboard", component: AffiliateDashboard },
+
+];
+
+export const publicRoutes = [
+   // Authentication Page
+   { name: "Logout", component: Logout },
+   { name: "connexion", component: Login },
+//    { path: "/en/login", component: LoginEn },
+   { name: "ForgetPasswordPage", component: ForgetPasswordPage },
+   { name: "Activate", component: Activate },
+   { name: "Admin", component: LoginAdmin },
+
+ //AuthenticationInner pages
+ { name: "BasicSignIn", component: BasicSignIn },
+ { name: "/CoverSignInr", component: CoverSignIn },
+ { name: "BasicSignUp", component: BasicSignUp },
+ { name: "CoverSignUp", component: CoverSignUp },
+//  { path: "/en/register", component: CoverSignUpEn },
+ { name: "CoverSignUp", component: AffiliateSignUp },
+//  { path: "/en/partners", component: AffiliateSignUpEn },
+ { name: "connexion", component: AffiliateSignIn  },
+//  { path: "/affiliate/en/login", component: AffiliateSignInEn  },
+ { name: "BasicPasswReset", component: BasicPasswReset },
+ { name: "CoverPasswReset", component: CoverPasswReset },
+ { name: "BasicLockScreen", component: BasicLockScreen },
+ { name: "CoverLockScreen", component: CoverLockScreen },
+ { name: "BasicLogout", component: BasicLogout },
+ { name: "CoverLogout", component: CoverLogout },
+ { name: "BasicSuccessMsg", component: BasicSuccessMsg },
+ { name: "SuccessMsg", component: SuccessMsg },
+//  { path: "/auth-success-msg-en", component: SuccessMsgEn },
+ { name: "CoverSuccessMsg", component: CoverSuccessMsg },
+ { name: "BasicTwosVerify", component: BasicTwosVerify },
+ { name: "CoverTwosVerify", component: CoverTwosVerify },
+ { name: "Basic404", component: Basic404 },
+ { name: "Cover404", component: Cover404 },
+ { name: " Alt404", component: Alt404 },
+ { name: "Error500", component: Error500 },
+ { name: "BasicPasswCreate", component: BasicPasswCreate },
+ { name: "CoverPasswCreate", component: CoverPasswCreate },
+ { name: "Offlinepage", component: Offlinepage },
+];
+
+export { authProtectedRoutes, publicRoutes, affiliateProtectedRoutes };
